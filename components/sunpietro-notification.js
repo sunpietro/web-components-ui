9e9d4ae3da1b900f5e885a9280b1a15ab071b39e
sunpietro <poczta@piotrnalepa.pl>
2019-09-10 22:16:22 +0200

    Updated notification component to handle slots and closing it

--- components/sunpietro-notification.js
+++ components/sunpietro-notification.js
@@ -1,11 +1,4 @@
 const template = document.createElement('template');
-const types = ['default', 'red', 'green', 'blue'];
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
 
 template.innerHTML = `
 <style>
@@ -34,11 +27,11 @@ template.innerHTML = `
         background: none;
     }
 </style>
-<div id="container">
-    <h3>Notificaton - <small>Web Component</small></h3>
-    <button type="button" id="random-type">Make it random</button>
-    <button type="button" id="red-type">Make it red</button>
-</div>`;
+<section id="container">
+    <slot name="title"><h3>Notification title</h3></slot>
+    <div id="message"><slot>Sample message</slot></div>
+    <button type="button" id="close">&times;</button>
+</section>`;
 
 class Notification extends HTMLElement {
     constructor() {
@@ -46,33 +39,28 @@ class Notification extends HTMLElement {
         this.attachShadow({ mode: 'open' });
 
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-        this.btnRandomType = this.shadowRoot.querySelector('#random-type');
-        this.btnRedType = this.shadowRoot.querySelector('#red-type');
-        this.container = this.shadowRoot.querySelector('#container');
-        this.container.setAttribute('type', 'default');
-
-        this.btnRandomType.addEventListener('click', () => {
-            console.log('Make it random - click');
-
-            this.container.setAttribute(
-                'type',
-                types[getRandomInt(0, types.length - 1)]
-            );
-        });
-
-        this.btnRedType.addEventListener('click', () => {
-            console.log('Make it red - click');
-
-            this.container.setAttribute('type', 'red');
-        });
     }
 
     static get observedAttributes() {
         return ['type'];
     }
 
-    connectedCallback() {}
+    _onClose() {
+        this.dispatchEvent(
+            new CustomEvent('close', { bubbles: true, detail: null })
+        );
+    }
+
+    connectedCallback() {
+        this.btnClose = this.shadowRoot.querySelector('#close');
+        this.container = this.shadowRoot.querySelector('#container');
+
+        this.btnClose.addEventListener('click', this._onClose, false);
+    }
+
+    disconnectedCallback() {
+        this.btnClose.removeEventListener('click', this._onClose);
+    }
 
     attributesChangedCallback(name, oldVal, newVal) {
         console.log({ oldVal, newVal, name });
